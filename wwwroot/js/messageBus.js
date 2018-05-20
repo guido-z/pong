6daa3eadd448a25352ce9c9ef01c01f333c902d9
Guido Zaglul <guido.zaglul@gmail.com>
2018-05-20 19:25:53 -0300

    add MessageBus

---
+++ wwwroot/js/messageBus.js
@@ -0,0 +1,18 @@
+﻿const { Observable, fromEvent } = rxjs;
+
+class MessageBus {
+    constructor() {        
+        this._events = {
+            keyDown: fromEvent(document, 'keydown')
+        };
+    }
+
+    static get instance() {
+        this._instance = this._instance || new MessageBus();
+        return this._instance;
+    }
+
+    subscribe(event) {
+        return this._events[event];
+    }
+}
