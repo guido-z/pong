0eb825e40a9ce2b6fa27f319ebb8ad3cd014efde
Guido Zaglul <guido.zaglul@gmail.com>
2018-05-20 15:50:39 -0300

    add Game class and main loop

--- wwwroot/scripts.js
+++ wwwroot/scripts.js
@@ -2,5 +2,37 @@
     const canvas = document.querySelector('canvas');
     const ctx = canvas.getContext('2d');
 
-    ctx.fillRect(0, 0, 800, 400);
+    const configuration = {
+        backgroundColor: 'black',
+        viewPort: {
+            height: canvas.getAttribute('height'),
+            width: canvas.getAttribute('width')
+        },
+        targetFramerate: 16
+    };
+
+    const game = new Game(configuration, ctx);
+    game.run();
 };
+
+class Game {
+    constructor(configuration, ctx) {
+        this._configuration = configuration;
+        this._ctx = ctx;
+    }
+
+    run() {
+        setInterval(() => {
+            this._update();
+            this._draw();
+        }, this._configuration.targetFramerate);
+    }
+
+    _update() { }
+
+    _draw() {
+        this._ctx.clearRect(0, 0, this._configuration.viewPort.width, this._configuration.viewPort.height);
+        this._ctx.fillStyle = this._configuration.backgroundColor;
+        this._ctx.fillRect(0, 0, this._configuration.viewPort.width, this._configuration.viewPort.height);
+    }
+}
