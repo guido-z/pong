d8b463362f6986acaf46417232e6076941e074a4
Guido Zaglul <guido.zaglul@gmail.com>
2018-05-20 17:43:23 -0300

    group .js and .css files in folders

---
+++ wwwroot/js/entities.js
@@ -0,0 +1,23 @@
+﻿class Paddle {
+    constructor(position) {
+        this._position = position;
+        this._height = 100;
+        this._width = 20;
+    }
+
+    update() { }
+
+    draw(ctx) {
+        ctx.save();
+        ctx.fillStyle = 'white';
+        ctx.fillRect(this._position.x, this._position.y, this._width, this._height);
+        ctx.restore();
+    }
+}
+
+class Position {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
--- wwwroot/js/game.js
+++ wwwroot/js/game.js
@@ -30,27 +30,3 @@
         this._paddle2.draw(this._ctx);
     }
 }
-
-class Paddle {
-    constructor(position) {
-        this._position = position;
-        this._height = 100;
-        this._width = 20;
-    }
-
-    update() { }
-
-    draw(ctx) {
-        ctx.save();
-        ctx.fillStyle = 'white';
-        ctx.fillRect(this._position.x, this._position.y, this._width, this._height);
-        ctx.restore();
-    }
-}
-
-class Position {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-}
